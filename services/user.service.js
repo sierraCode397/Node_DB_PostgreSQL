18222290659678bec4db859ef970e8fd88f48973
izaack107 <izaack107@gmail.com>
2023-09-22 19:38:37 -0600

    Primer modelo en Sequelize

--- services/user.service.js
+++ services/user.service.js
@@ -1,11 +1,9 @@
 const boom = require('@hapi/boom');
-const pool = require('../libs/postgres_pool');
 
+const { models } = require('./../libs/sequelize')
 
 class UserService {
   constructor() {
-    this.pool = pool;
-    this.pool.on('error', (err) => console.error(err));
   }
 
   async create(data) {
@@ -13,9 +11,8 @@ class UserService {
   }
 
   async find() {
-    const query = 'SELECT * FROM task';
-    const rta = await this.pool.query(query);
-    return rta.rows;
+    const rta = await models.User.findAll();
+    return rta;
   }
 
   async findOne(id) {
