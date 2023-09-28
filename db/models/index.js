3c9e332383cbf930c6b489a02208203cfa8b9b1a
izaack107 <izaack107@gmail.com>
2023-09-28 09:24:30 -0600

    Creando una entidad para hacer relacion 1 - 1 [1.0]

--- db/models/index.js
+++ db/models/index.js
@@ -2,13 +2,14 @@ const { User, UserSchema } = require('./user.model');
 const { Product, ProductSchema } = require('./product.model');
 const { Order, OrderSchema } = require('./order.model');
 const { Categorie, CategorieSchema } = require('./categories.model');
-
+const { Customer, CustomerSchema } = require('./customer.model');
 
 function setupModels(sequelize) {
   User.init(UserSchema, User.config(sequelize));
   Product.init(ProductSchema, Product.config(sequelize));
   Order.init(OrderSchema, Order.config(sequelize));
   Categorie.init(CategorieSchema, Categorie.config(sequelize));
+  Customer.init(CustomerSchema, Customer.config(sequelize));
 }
 
 module.exports = setupModels;
---
+++ schemas/customer.schema.js
@@ -0,0 +1,24 @@
+const Joi = require('joi');
+
+const id = Joi.number().integer();
+const name = Joi.string().min(3).max(30);
+const lastName = Joi.string();
+const phone =  Joi.string();
+
+const getCustomerSchema = Joi.object({
+  id: id.required(),
+});
+
+const createCustomerSchema = Joi.object({
+  name: name.required(),
+  lastName: lastName.required(),
+  phone: phone.required()
+});
+
+const updateCustomerSchema = Joi.object({
+  name,
+  lastName,
+  phone
+});
+
+module.exports = { getCustomerSchema, createCustomerSchema, updateCustomerSchema };
