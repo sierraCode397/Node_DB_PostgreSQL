f69af9f26d04f523cacc4b6ca8b473d2b5068b4e
izaack107 <izaack107@gmail.com>
2023-10-03 00:45:22 -0600

    [ADD] Resolviendo relaciones muchos a muchos

--- db/models/order.model.js
+++ db/models/order.model.js
@@ -27,7 +27,7 @@ const OrderSchema = {
     field: 'create_at',
     defaultValue: Sequelize.NOW
   },
-/*   total: {
+  total: {
     type: DataTypes.VIRTUAL,
     get(){
       if(this.items.length > 0){
@@ -37,7 +37,7 @@ const OrderSchema = {
       }
       return 0;
     }
-  } */
+  }
 }
 
 class Order extends Model {
